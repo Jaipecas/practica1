920c3f7c805cb4abcf5b338cb0ecb2fc863cbc07
Jaime Perez <jaipecas89@gmail.com>
2020-10-21 12:33:04 +0200

    Cambios en el ejercicio 4

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,10 +1,8 @@
 cadena = input("Introduzca una palabra: ")
 totalMayus = 0
-i = 0
 
 for caracter in cadena:
-    if cadena[i].isupper():
+    if caracter.isupper():
         totalMayus += 1
-    i += 1
 
 print(f"El número de letras mayúsculas presentes en la palabra introducida son: {totalMayus}")
